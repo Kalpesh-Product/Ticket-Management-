884b09503922d5f10e1b268aef1d38ef0d8788c8
AllanMScode <97829785+AllanMScode@users.noreply.github.com>
2024-09-04 15:00:23 +0530

    manual assigning of member doesn't work

--- src/pages/AdminTickets.jsx
+++ src/pages/AdminTickets.jsx
@@ -7,27 +7,133 @@ import ExtraSpace from "../components/ExtraSpace";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-import { Button } from "flowbite-react";
+import { Button, Modal } from "flowbite-react";
 import LayoutAdmin from "./../components/Layout/LayoutAdmin";
 
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const AdminTickets = () => {
-  const [tickets, setTickets] = useState(null);
+  // array containing tickets (from tickets collection)
+  const [tickets, setTickets] = useState([]);
+
+  const [openModal, setOpenModal] = useState(false);
+
+  // array containing members (from members collection)
+  const [members, setMembers] = useState([]);
+
+  // form to update assignedMember in ticket DB
+  const [assignMemberForm, setAssignMemberForm] = useState({
+    _id: null,
+    assignedMember: "",
+  });
 
   // Function to get all tickets
   const fetchTickets = async () => {
     try {
       const responseFromBackend = await axios.get("/get-all-tickets");
 
+      console.log(responseFromBackend.data.tickets);
       setTickets(responseFromBackend.data.tickets);
     } catch (error) {
       console.log(error);
+      setTickets([]);
     }
   };
 
   // UseEffect to view tickets
   useEffect(() => {
     fetchTickets();
+    fetchMembers();
   }, []);
+
+  // Function to get all members
+  const fetchMembers = async () => {
+    try {
+      const responseFromBackend = await axios.get("/get-all-members");
+
+      console.log(responseFromBackend.data.members); // Log the data to verify
+      setMembers(responseFromBackend.data.members);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // View  details before delete
+  const toggleAssignMemberForm = (
+    ticketToBeDisplayedBeforeEditingAssignedMember
+  ) => {
+    // this function should preload the state with the values of the ticket we're editing
+    // Get the current ticket
+    // console.log(ticket);
+    // Set state on update form
+    setAssignMemberForm({
+      assignedMember:
+        ticketToBeDisplayedBeforeEditingAssignedMember.assignedMember,
+
+      _id: ticketToBeDisplayedBeforeEditingAssignedMember._id,
+    });
+
+    console.log(assignMemberForm);
+  };
+
+  const assignMember = async (e) => {
+    try {
+      e.preventDefault();
+      // User's selected reason
+      const assignedMember = assignMemberForm.assignedMember;
+
+      const responseFromBackend = await axios.put(
+        `/update-assign-member/${assignMemberForm._id}`,
+        {
+          assignedMember: assignedMember,
+        }
+      );
+      // console.log(responseFromBackend);
+
+      // Update state
+      // creating a duplicate of the tickets
+      const newTickets = [...tickets];
+      const ticketIndex = tickets.findIndex((ticket) => {
+        return ticket._id === assignMemberForm._id; // finds the index of the ticket which is updated (ticket whose id was in the button). We find the index so that we can update the ticket at that index
+      });
+
+      newTickets[ticketIndex] = responseFromBackend.data.ticket; // The ticket at that particular index is now equal to the response we got from updating the ticket
+
+      setTickets(newTickets);
+
+      setAssignMemberForm({
+        _id: null,
+
+        assignedMember: "",
+      });
+
+      toast.success("Member assigned successfully.");
+      // Optionally, update the members state to remove the deleted member
+
+      setOpenModal(false);
+      fetchMembers();
+    } catch (error) {
+      console.error("Error assigning member:", error);
+      toast.error("Failed to assign member.");
+    }
+  };
+
+  const updateAssignFormField = (e) => {
+    // console.log("typed");
+
+    const target = e.target;
+    const name = target.name;
+    const value = target.value;
+
+    setTickets({
+      ...assignMemberForm,
+      [name]: value,
+    });
+
+    // console.log({ name, value });
+    console.log(assignMemberForm);
+  };
 
   return (
     <LayoutAdmin>
@@ -35,6 +141,66 @@ const AdminTickets = () => {
         {/* <AdminDashboard /> */}
         {/* <br />
         <br /> */}
+
+        {/* Modal Start */}
+        <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
+          {/* <form onSubmit={deleteTicket}> */}
+          <form onSubmit={assignMember}>
+            <Modal.Header>Assign Member</Modal.Header>
+            <Modal.Body>
+              <div className="space-y-6">
+                {/* <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                    Sign in to our platform
+                  </h3> */}
+                <div>
+                  <label
+                    htmlFor="assignedMember"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Select a member
+                  </label>
+                  <select
+                    name="assignedMember"
+                    onChange={updateAssignFormField}
+                    // value={assignMemberForm.assignedMember}
+                    id="assignedMember"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option>Select</option>
+                    {members &&
+                      members.map((member) => {
+                        return (
+                          <option key={member._id} value={member.email}>
+                            {member.name}
+                          </option>
+                        );
+                      })}
+                    {/* <option value="a@email.com">Member A</option>
+                    <option value="b@email.com">Member B</option>
+                    <option value="c@email.com">Member C</option>
+                    <option value="d@email.com">Member D</option>
+                    <option value="e@email.com">Member E</option>
+                    <option value="f@email.com">Member F</option>
+                    <option value="h@email.com">Member G</option>
+                    <option value="g@email.com">Member H</option> */}
+                  </select>
+                </div>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                type="submit"
+                color="blue"
+                // onClick={() => setOpenModal(false)}
+                // onClick={deleteTicket}
+              >
+                Assign
+              </Button>
+              <Button color="gray" onClick={() => setOpenModal(false)}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </form>
+        </Modal>
+
         <h2 className="mb-5 text-2xl text-center font-bold text-gray-900 dark:text-white">
           List Of Tickets Raised
         </h2>
@@ -140,7 +306,11 @@ const AdminTickets = () => {
                             size="xs"
                             color="blue"
                             // onClick={() => handleAssignMember(ticket._id)}
-                          >
+                            // onClick={() => setOpenModal(true)}
+                            onClick={() => {
+                              toggleAssignMemberForm(ticket);
+                              setOpenModal(true);
+                            }}>
                             Assign Member
                           </Button>
                         ) : (
--- src/pages/MemberAcceptedTickets.jsx
+++ src/pages/MemberAcceptedTickets.jsx
@@ -4,14 +4,25 @@ import MemberDashboardButton from "../components/MemberDashboardButton";
 import MiddleBody from "../components/Layout/MiddleBody";
 import ExtraSpace from "../components/ExtraSpace";
 import axios from "axios";
-import { Button } from "flowbite-react";
+import { Button, Modal } from "flowbite-react";
 import { useParams } from "react-router-dom";
 import LayoutMemberLoggedIn from "../components/Layout/LayoutMemberLoggedIn";
+
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MemberAcceptedTickets = () => {
   const params = useParams();
 
   const [tickets, setTickets] = useState(null);
+
+  const [openModal, setOpenModal] = useState(false);
+
+  // state to hold the values of inputs in cannot resolve popup
+  const [cannotResolveForm, setCannotResolveForm] = useState({
+    _id: null,
+    memberMessageToAdmin: "",
+  });
 
   // Function to get all tickets
   const fetchTickets = async () => {
@@ -50,22 +61,120 @@ const MemberAcceptedTickets = () => {
     setTickets(newTickets);
   };
 
-  const cannotResolve = async (idOfTheTicketToBeClosed) => {
-    const responseFromBackend = await axios.put(
-      `/cannot-resolve-ticket/${idOfTheTicketToBeClosed}`
-    );
-    // console.log(responseFromBackend);
+  // const MemberCannotResolveTicket = async (
+  //   idOfTheTicketWhichCannotBeResolved
+  // ) => {
+  //   const memberMessageToAdmin = cannotResolveForm.memberMessageToAdmin;
 
-    // Update state
-    // creating a duplicate of the tickets
-    const newTickets = [...tickets];
-    const ticketIndex = tickets.findIndex((ticket) => {
-      return ticket._id === idOfTheTicketToBeClosed; // finds the index of the ticket which is updated (ticket whose id was in the button). We find the index so that we can update the ticket at that index
+  //   const responseFromBackend = await axios.put(
+  //     `/member-cannot-resolve-ticket/${idOfTheTicketWhichCannotBeResolved}`,
+  //     {
+  //       memberMessageToAdmin: memberMessageToAdmin,
+  //     }
+  //   );
+  //   // console.log(responseFromBackend);
+
+  //   // Update state
+  //   // creating a duplicate of the tickets
+  //   const newTickets = [...tickets];
+  //   const ticketIndex = tickets.findIndex((ticket) => {
+  //     return ticket._id === idOfTheTicketWhichCannotBeResolved; // finds the index of the ticket which is updated (ticket whose id was in the button). We find the index so that we can update the ticket at that index
+  //   });
+
+  //   newTickets[ticketIndex] = responseFromBackend.data.ticket; // The ticket at that particular index is now equal to the response we got from updating the ticket
+
+  //   setTickets(newTickets);
+  //   // fetchTickets();
+  //   setOpenModal(false);
+
+  //   setCannotResolveForm({
+  //     _id: null,
+  //     memberMessageToAdmin: "",
+  //   });
+  // };
+
+  const MemberCannotResolveTicket = async (e) => {
+    try {
+      e.preventDefault();
+
+      // User's selected reason
+      // const userDepartment = updateForm.userDepartment;
+      // const userMessage = updateForm.userMessage;
+
+      const memberMessageToAdmin = cannotResolveForm.memberMessageToAdmin;
+
+      // const responseFromBackend = await axios.put(
+      //   `/user-edit-ticket/${updateForm._id}`,
+      //   {
+      //     userDepartment: userDepartment,
+      //     userMessage: userMessage,
+      //   }
+      // );
+
+      const responseFromBackend = await axios.put(
+        `/member-cannot-resolve-ticket/${cannotResolveForm._id}`,
+        {
+          memberMessageToAdmin: memberMessageToAdmin,
+        }
+      );
+      // console.log(responseFromBackend);
+
+      // Update state
+      // creating a duplicate of the tickets
+      const newTickets = [...tickets];
+      const ticketIndex = tickets.findIndex((ticket) => {
+        return ticket._id === cannotResolveForm._id; // finds the index of the ticket which is updated (ticket whose id was in the button). We find the index so that we can update the ticket at that index
+      });
+
+      newTickets[ticketIndex] = responseFromBackend.data.ticket; // The ticket at that particular index is now equal to the response we got from updating the ticket
+
+      setTickets(newTickets);
+
+      setCannotResolveForm({
+        _id: null,
+        memberMessageToAdmin: "",
+      });
+
+      toast.success("Message sent successfully.");
+      // Optionally, update the tickets state to remove the updated ticket
+
+      setOpenModal(false);
+    } catch (error) {
+      console.error("Error sending message:", error);
+      toast.error("Failed to send.");
+    }
+  };
+
+  // const submitCannotResolveForm = (e) => {
+  //   e.preventDefault();
+
+  //   MemberCannotResolveTicket();
+  // };
+
+  // Function to update the state (ticket object) as the user types
+  const updateCannotResolveFormField = (e) => {
+    // console.log("typed");
+
+    const target = e.target;
+    const name = target.name;
+    const value = target.value;
+
+    setCannotResolveForm({
+      ...cannotResolveForm,
+      [name]: value,
     });
 
-    newTickets[ticketIndex] = responseFromBackend.data.ticket; // The ticket at that particular index is now equal to the response we got from updating the ticket
+    // console.log({ name, value });
+    // console.log(cannotResolveForm);
+  };
 
-    setTickets(newTickets);
+  const toggleCannotResolveForm = (ticketToBeDisplayed) => {
+    // Set state on update form
+    setCannotResolveForm({
+      memberMessageToAdmin: ticketToBeDisplayed.memberMessageToAdmin,
+
+      _id: ticketToBeDisplayed._id,
+    });
   };
 
   return (
@@ -74,6 +183,53 @@ const MemberAcceptedTickets = () => {
         <h2 className="mb-4 text-2xl text-center font-bold text-gray-900 dark:text-white">
           Member Dashboard
         </h2>
+
+        {/* Modal Start */}
+        <form>
+          <Modal
+            dismissible
+            show={openModal}
+            onClose={() => setOpenModal(false)}>
+            {/* <form onSubmit={deleteTicket}> */}
+            {/* <form onSubmit={submitCannotResolveForm}> */}
+            <Modal.Header>Please Explain The Issue</Modal.Header>
+            <Modal.Body>
+              <div className="space-y-6">
+                {/* <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                    Sign in to our platform
+                  </h3> */}
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="memberMessageToAdmin"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Enter Message For The Admin
+                  </label>
+                  <textarea
+                    onChange={updateCannotResolveFormField}
+                    value={cannotResolveForm.memberMessageToAdmin}
+                    name="memberMessageToAdmin"
+                    id="memberMessageToAdmin"
+                    rows={8}
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Please enter your message here"
+                  />
+                </div>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                type="submit"
+                color="blue"
+                // onClick={() => setOpenModal(false)}
+                onClick={MemberCannotResolveTicket}>
+                Sumbit Message
+              </Button>
+              <Button color="gray" onClick={() => setOpenModal(false)}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </form>
         {/* <MemberDashboardButton />
         <br /> */}
         <br />
@@ -182,7 +338,11 @@ const MemberAcceptedTickets = () => {
                         scope="row"
                         className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         <Button
-                          onClick={() => cannotResolve(ticket._id)}
+                          // onClick={() => cannotResolve(ticket._id)}
+                          onClick={() => {
+                            setOpenModal(true);
+                            toggleCannotResolveForm(ticket);
+                          }}
                           size="xs"
                           color="failure">
                           Cannot Resolve

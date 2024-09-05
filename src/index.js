c03ab1f37cd32c2b0e58f96323d6b6a3d3dd64ff
AllanMScode <97829785+AllanMScode@users.noreply.github.com>
2024-09-05 15:18:29 +0530

    added axios changes in index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.baseURL = "https://ticket-management-backend-two.vercel.app";
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);

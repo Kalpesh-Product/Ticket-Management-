84ca75e0dcab05bf982fe681fa216fd1ac206580
AllanMScode <97829785+AllanMScode@users.noreply.github.com>
2024-09-05 13:25:24 +0530

    changed link

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080";
+// axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = "https://ticket-management-backend-two.vercel.app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);

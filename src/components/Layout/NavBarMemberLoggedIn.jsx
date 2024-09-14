3b9e32e33347c2aca47951437d30d3298fb6cbdc
AllanMScode <97829785+AllanMScode@users.noreply.github.com>
2024-09-14 17:35:50 +0530

    member availability

--- src/components/Layout/NavBarMemberLoggedIn.jsx
+++ src/components/Layout/NavBarMemberLoggedIn.jsx
@@ -2,9 +2,66 @@ import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
 import { Link, useParams } from "react-router-dom";
 import { DarkThemeToggle } from "flowbite-react";
 import logo from "../../images/WONO LOGO white TP.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function NavBarMemberLoggedIn() {
   const params = useParams();
+
+  const [memberDetails, setMemberDetails] = useState({
+    availability: "",
+  });
+
+  const loggedInMemberEmail = params.email;
+  // console.log(loggedInMemberEmail);
+
+  const viewMemberDetails = async () => {
+    const responseFromBackend = await axios.get(
+      `/view-member-availability/${loggedInMemberEmail}`
+    );
+
+    // console.log(responseFromBackend);
+    // console.log(responseFromBackend.data.member.availability);
+
+    setMemberDetails({
+      availability: responseFromBackend.data.member.availability,
+    });
+
+    // console.log(memberDetails);
+  };
+
+  // useeffect for running the above function
+  useEffect(() => {
+    viewMemberDetails();
+    // console.log(memberDetails);
+  }, []);
+
+  const changeToUnavailable = async () => {
+    const responseFromBackend = await axios.put(
+      `/member-changes-to-unavailable/${loggedInMemberEmail}`
+    );
+
+    setMemberDetails({
+      availability: responseFromBackend.data.member.availability,
+    });
+
+    viewMemberDetails();
+
+    // console.log(responseFromBackend);
+    // console.log(responseFromBackend.data.member.availability);
+  };
+
+  const changeToAvailable = async () => {
+    const responseFromBackend = await axios.put(
+      `/member-changes-to-available/${loggedInMemberEmail}`
+    );
+
+    setMemberDetails({
+      availability: responseFromBackend.data.member.availability,
+    });
+
+    viewMemberDetails();
+  };
 
   return (
     <Navbar fluid rounded className="sticky top-0 z-50">
@@ -28,9 +85,23 @@ export function NavBarMemberLoggedIn() {
                 {params.email}
               </span>
             </Dropdown.Header>
-            <Dropdown.Item>Status: Available</Dropdown.Item>
             <Dropdown.Item>
-              <Button size="xs">Mark as Unavailable</Button>
+              Status:{" "}
+              {memberDetails.availability == "Available"
+                ? "Available"
+                : "Unavailable"}
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+              {memberDetails.availability == "Available" ? (
+                <Button size="xs" color="failure" onClick={changeToUnavailable}>
+                  Mark as Unavailable
+                </Button>
+              ) : (
+                <Button size="xs" color="success" onClick={changeToAvailable}>
+                  Mark as Available
+                </Button>
+              )}
             </Dropdown.Item>
             {/* <Dropdown.Item as={Link} to="/change-password">
             Change Password
